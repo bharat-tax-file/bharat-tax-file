e92de669ba3460d8c8950efa429e92a6174984ee
bharat-tax-file <easyreturn@bharattaxfile.com>
2025-03-30 10:17:14 +0530

    about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default function Page() {
       <InnerPageContainer title="About Us">
             <PageMetaTags title="About Us" description={"GST and ITR Filing Services"} url="/about-us"/>
             <p className="mt-8">
-            Welcome to Bharat Tax file , your trusted partner for hassle-free GST and ITR filing! We specialize in making tax compliance easy, efficient, and stress-free for businesses and individuals. Our expert team is committed to providing accurate and timely tax solutions tailored to your needs.
+            Welcome to Bharat Tax File, your trusted partner for hassle-free GST and ITR filing! We specialize in making tax compliance easy, efficient, and stress-free for businesses and individuals. Our expert team is committed to providing accurate and timely tax solutions tailored to your needs.
             </p>
 
             <h3 className="mt-8 font-bold text-xl">Our Mission</h3>
@@ -15,19 +15,18 @@ export default function Page() {
             </p>
             
             <h3 className="mt-8 font-bold text-xl">Why Choose Us?</h3>
-            <p className="mt-2">
-             
-             1. Flexible hours consultancy 
-             2. Personalized consultancy 
-             3. Expert knowledge  
-             4. Stress free tax filing
-             5. Transparent pricing 
-             6. Timely services             
-            </p>
+            <ul className="mt-2 list-disc list-inside">
+              <li>Flexible hours consultancy</li>
+              <li>Personalized consultancy</li>
+              <li>Expert knowledge</li>
+              <li>Stress-free tax filing</li>
+              <li>Transparent pricing</li>
+              <li>Timely services</li>
+            </ul>
             
             <h3 className="mt-8 font-bold text-xl">Get Started Today!</h3>
             <p className="mt-2">
-            Join thousands of satisfied customers who trust us for their tax compliance needs. Whether you're a business owner or an individual taxpayer, we’re here to make GST and ITR filing effortless for you. Let us handle the paperwork while you focus on what truly matters!
+            Join thousands of satisfied customers who trust us for their tax compliance needs. Whether you&apos;re a business owner or an individual taxpayer, we’re here to make GST and ITR filing effortless for you. Let us handle the paperwork while you focus on what truly matters!
             </p>
       </InnerPageContainer>
     )
