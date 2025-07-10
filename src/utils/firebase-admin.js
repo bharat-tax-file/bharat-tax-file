1a7ec101a00825ef7b7fe6d1ffdc0b25f7a2ab80
Dev-shannn <nishantpandeyy07@gmail.com>
2025-07-10 22:27:37 +0530

    firebase

--- src/utils/firebase-admin.js
+++ src/utils/firebase-admin.js
@@ -1,13 +1,13 @@
-// src/utils/firebase-admin.js
-
 import * as admin from 'firebase-admin';
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey,
     }),
   });
 }
