33fd3c06a894ead99901be9134b0af3911ba1eb1
Dev-shannn <nishantpandeyy07@gmail.com>
2025-07-10 22:01:13 +0530

    1stphase

--- src/utils/firebase-admin.js
+++ src/utils/firebase-admin.js
@@ -1,10 +1,14 @@
-import admin from 'firebase-admin';
-import serviceAccount from './firebase-admin.json'; // path relative to this file
+// src/utils/firebase-admin.js
+
+import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://bharat-tax-file.firebaseio.com", // optional
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
